78f633723583ccc1ea790bf9dcbb5baed8ac9fe8
AbuZaferMdFahim <mightyanim007@gmail.com>
2023-09-10 14:22:23 +0600

    rest_framework setup, create serializers, viewset and router and documenting api

--- notebook/settings.py
+++ notebook/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes.apps.NotesConfig'
+    'notes.apps.NotesConfig',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Note
+from rest_framework import viewsets
+from .serializers import NoteSerializer 
 # Create your views here.
+class NotesViewSets(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
